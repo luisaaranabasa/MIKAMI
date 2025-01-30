89c1af259e1ec172a1f9048d40ddef7fe5bfac65
luisaaranabasa <luisa.arana2003@gmail.com>
2025-01-30 20:11:27 +0100

    h

--- js/script.js
+++ js/script.js
@@ -6,3 +6,21 @@ document.addEventListener('DOMContentLoaded', () => {
     menuItems.classList.toggle('active');
   });
 });
+
+function cambiarVideo() {
+  var video = document.getElementById("homeVideo");
+  var source = video.getElementsByTagName("source")[0];
+
+  if (window.innerWidth < 768) {
+      source.src = "css/media/video/homeresponsive.mp4";
+  } else {
+      source.src = "css/media/video/home.mp4";
+  }
+
+  video.load(); // Recarga el video con la nueva fuente
+}
+
+// Ejecutar cuando la página cargue y cuando se redimensione la ventana
+window.onload = cambiarVideo;
+window.onresize = cambiarVideo;
+
