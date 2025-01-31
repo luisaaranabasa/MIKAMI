e7fb8969bcf5f102409a7e9b105f74ca7a29baed
luisaaranabasa <luisa.arana2003@gmail.com>
2025-01-31 18:09:01 +0100

    menu hamburguesa

--- js/script.js
+++ js/script.js
@@ -1,26 +1,11 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const menuToggle = document.getElementById('menuToggle');
-  const menuItems = document.getElementById('menuItems');
+document.addEventListener("DOMContentLoaded", function () {
+  const menuToggle = document.getElementById("menuToggle");
+  const menuItems = document.getElementById("menuItems");
 
-  menuToggle.addEventListener('click', () => {
-    menuItems.classList.toggle('active');
+  menuToggle.addEventListener("click", function () {
+      menuItems.classList.toggle("active");
+      menuToggle.classList.toggle("active"); // Rota el símbolo azul
   });
 });
 
-function cambiarVideo() {
-  var video = document.getElementById("homeVideo");
-  var source = video.getElementsByTagName("source")[0];
-
-  if (window.innerWidth < 768) {
-      source.src = "css/media/video/homeresponsive.mp4";
-  } else {
-      source.src = "css/media/video/home.mp4";
-  }
-
-  video.load(); // Recarga el video con la nueva fuente
-}
-
-// Ejecutar cuando la página cargue y cuando se redimensione la ventana
-window.onload = cambiarVideo;
-window.onresize = cambiarVideo;
 
